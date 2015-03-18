03f8e41b9083f90a00b798f49228dba7d0fe6196
Aaron Szerencses <as@imko.de>
2015-03-18 09:13:52 +0100

    Initial commit.
    Signed-off-by: Aaron Szerencses <as@imko.de>

---
+++ docker/postgres/setup-confluence-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "******CREATING CONFLUENCE DATABASE******"
+gosu postgres postgres --single <<- EOSQL
+   CREATE DATABASE confluence WITH ENCODING 'UNICODE' LC_COLLATE 'C' \
+       LC_CTYPE 'C' TEMPLATE template0;
+   CREATE USER confluence;
+   GRANT ALL PRIVILEGES ON DATABASE confluence to confluence;
+EOSQL
+echo ""
+
+{ echo; echo "host confluence confluence 0.0.0.0/0 trust"; } >> \
+  "$PGDATA"/pg_hba.conf
+
+if [ -r '/tmp/confluence.dump' ]; then
+    echo "**IMPORTING CONFLUENCE DATABASE BACKUP**"
+    gosu postgres postgres &
+    SERVER=$!; sleep 2
+    gosu postgres psql confluence < /tmp/confluence.dump
+    kill $SERVER; wait $SERVER
+    echo "**CONFLUENCE DATABASE BACKUP IMPORTED***"
+fi
+
+echo "******CONFLUENCE DATABASE CREATED******"
