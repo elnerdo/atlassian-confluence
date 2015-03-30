6fea4d74eb482bb6c357d24566937db5e2fdd4f7
Aaron Szerencses <as@imko.de>
2015-03-30 14:51:38 +0200

    Changes fabfile and README.md

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-## Atlassian Confluence
+## Atlassian confluence
 
 For more information on the app please refere to the offical
 Atlassian websites:
 
 
-- [Confluence](https://www.atlassian.com/software/confluence)
+- [confluence](https://www.atlassian.com/software/confluence)
 
 ### Prerequisites
 
@@ -22,7 +22,7 @@ TBD
     $ docker-compose logs
 
 If you deploy the app for the first time you may need to restore the database
-from a backup for each app and adapt the database connection settings!
+from a backup!
 
 ### Debug (aka. go inside) an image
 
@@ -34,7 +34,10 @@ from a backup for each app and adapt the database connection settings!
 If you start this orchestration for the first time, a handy feature is to
 import your old data. If you're e.g. moving everything to another server
 you can put your database backups into the tmp folder and the db initscript
-will pick them up automagically on the first run.
+will pick them up automagically on the first run if there is no `data` folder
+
+
+    1. Approach with a dump file and tgz archive
 
     # move your confluence db backup file to tmp (filename is important).
     $ mv confluence.dump tmp/confluence.dump
@@ -42,21 +45,58 @@ will pick them up automagically on the first run.
     # unpack your confluence-home backup archive
     $ tar xzf confluence-home.tgz --strip=1 -C home
 
-### Backup the home folders
 
-    $ mkdir -p backup/$(date +%F)
-      tar czf backup/$(date +%F)/confluence-home.tgz home
+    2. Approach with bup
 
-### Backup the PostgreSQL data
+    # remove theses folder if present:
+    $ data/ tmp/ home/ dumps/
 
-    # backup the confluence database
-    $ docker run -it --rm --link atlassian_database_1:db -v $(pwd):/tmp \
-        postgres sh -c 'pg_dump -U confluence -h "$DB_PORT_5432_TCP_ADDR" \
-        -w confluence > /tmp/confluence.dump'
+    # move contents from the bup backup.
+    $ sudo mv confluencebackup/confluence/* /srv/data/confluence
 
 ### Restore the PostgreSQL data
 
-    # restore the confluence database backup
-    $ docker run -it --rm --link atlassian_database_1:db -v $(pwd):/tmp \
-        postgres sh -c 'pg_restore -U confluence -h "$DB_PORT_5432_TCP_ADDR" \
-        -n public -w -d confluence /tmp/confluence.dump'
+To restore the postresql data you have two options:
+
+    1. Remove the `data` folder and put the dumpfile into `tmp`
+    2. Replace the `data` folder with a bup backuped `data` folder
+
+## Restoring with the fabfile or bup
+
+    # All commands have to be launched from within the root folder of the app.
+    $ /srv/data/confluence
+
+Once you have run the restore command, you have to remove `data/ tmp/ dumps/ home/`
+Move the folders from the created confluencebackup folder into the app root (where the deleted folders used to be).
+
+### Show all possible restore points:
+
+    sudo BUP_DIR=$(pwd)/backup bup ls confluencebackup`
+
+### Restore latest backup:
+
+    # With the fabfile:
+    $ sudo fab restore
+
+    # Without the fabfile:
+    $ sudo BUP_DIR=$(pwd)/backup bup restore
+
+### Restore a specific backup:
+
+    # With the fabfile:
+    $ sudo fab restore:revision='2015-03-26-123711'
+
+    # Without the fabfile:
+    $ sudo BUP_DIR=$(pwd)/backup bup restore confluencebackup/2015-03-26-123711/$(pwd)
+
+### Restore to a different folder than confluencebackup
+
+By default bup will restore the backup to a folder called confluencebackup.
+To restore to another folder you can use the `-C` flag or pass `'destination'`
+to the fabfile.
+
+    # With the fabfile:
+    $ sudo fab restore:destination='anotherlocation'
+
+    # Without the fabfile:
+    $ sudo BUP_DIR=$(pwd)/backup bup restore -C anotherlocation confluencebackup/latest/$(pwd)
--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import boto.ec2
+import json
 from fabric.api import local, task
 from fabric.context_managers import shell_env
 
@@ -13,35 +15,55 @@ def backup():
     bup_init()
     bup_index()
     do_bup_backup()
+    do_snapshot()
     start_docker_container()
 
 
 @task
-def restore():
-    do_bup_restore()
+def restore(destination='confluencebackup', revision='latest'):
+    do_bup_restore(destination, revision)
+
+
+def do_snapshot():
+    with open('.aws-auth') as f:
+        auth = json.load(f)
+    os.environ['AWS_ACCESS_KEY_ID'] = auth['key_id']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = auth['secret_key']
+    instance_id = local(
+        'curl http://169.254.169.254/latest/meta-data/instance-id',
+        capture=True)
+    conn = boto.ec2.connect_to_region('eu-central-1')
+    volumes = conn.get_all_volumes(
+        filters={'attachment.instance-id': instance_id})
+
+    backup_vol = None
+    for vol in volumes:
+        if vol.attach_data.device == '/dev/xvdh':
+            backup_vol = vol
+            break
+
+    if backup_vol:
+        conn.create_snapshot(backup_vol.id, description='confluence-data-snap')
 
 
 def do_bup_backup():
     print('Running incremental backup')
-    backuped = local('BUP_DIR=$(pwd)/backup bup save -n confluencebackup '
-                     '$(pwd)')
+    backuped = local('BUP_DIR=$(pwd)/backup bup save -n confluencebackup $(pwd)')
     if backuped.return_code != 0:
         print 'An error occurred while backing up. Aborting...'
         sys.exit(1)
 
 
-def do_bup_restore():
-    restored = local('BUP_DIR=$(pwd)/backup bup restore -C confluencebackup '
-                     'confluencebackup/latest/$(pwd)')
+def do_bup_restore(destination, revision):
+    restored = local('BUP_DIR=$(pwd)/backup bup restore -C {0} '
+                     'confluencebackup/{1}/$(pwd)'.format(destination, revision))
     if restored.return_code != 0:
-        print 'An error occurred while restoring. Aborting...'
         sys.exit(1)
 
 
 def bup_init():
     initiated = local('BUP_DIR=$(pwd)/backup bup init')
     if initiated.return_code != 0:
-        print 'An error occurred while initiating bup. Aborting...'
         sys.exit(1)
 
 
@@ -49,7 +71,6 @@ def bup_index():
     indexed = local('BUP_DIR=$(pwd)/backup bup index --exclude-from '
                     '.bup.ignore $(pwd)')
     if indexed.return_code != 0:
-        print 'An error occurred while indexing bup. Aborting...'
         sys.exit(1)
 
 
@@ -57,30 +78,23 @@ def do_psql_dump():
     print 'dumping...'
     dumped = local('docker run -t --rm --link confluence_database_1:db '
                    '-v $(pwd):/tmp postgres sh -c \'pg_dump -U confluence -h '
-                   '"$DB_PORT_5432_TCP_ADDR" -w confluence > '
-                   '/tmp/confluence.dump\'')
+                   '"$DB_PORT_5432_TCP_ADDR" -w confluence > /tmp/confluence.dump\'')
 
     if dumped.return_code != 0:
-        print 'An error occured while dumping database. Aborting...'
         sys.exit(1)
-    print 'dumped...'
+
+    if not os.path.exists(os.path.join(os.getcwd(), 'dumps')):
+        local('mkdir dumps')
+    local('mv confluence.dump dumps/confluence.dump')
 
 
 def stop_docker_container():
-    print 'stoping...'
     stopped = local('sudo docker-compose stop confluence')
     if stopped.return_code != 0:
-        print 'An error occurred while stoping container confluence. \
-Aborting...'
         sys.exit(1)
-    print 'stopped...'
 
 
 def start_docker_container():
-    print 'starting...'
     started = local('sudo docker-compose start confluence')
     if started.return_code != 0:
-        print 'An error occurred while starting container confluence. \
-Aborting...'
         sys.exit(1)
-    print 'started...'
